05906ff5c63ec4046b27be817bd8fa25ec6707aa
Tpoc311 <tpoc311@yandex.ru>
2026-06-11 18:15:42 +0300

    Transforms import actualized

--- pytorch/image_classification/imagenet1000/test.py
+++ pytorch/image_classification/imagenet1000/test.py
@@ -9,7 +9,7 @@ from torchvision.datasets import ImageNet
 from torchvision.models import AlexNet, resnet18, resnet34, resnet50, resnet101, resnet152
 from tqdm import tqdm
 
-from src.transforms.alexnet import get_val_transforms
+from pytorch.image_classification.src.transforms import get_val_transforms
 from src.utils import decode_image
 
 
