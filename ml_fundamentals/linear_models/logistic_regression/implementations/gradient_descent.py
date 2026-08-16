5d558e9574c4664839bdbb5c9e16a2625f6e524e
Tpoc311 <TPOC311@yandex.ru>
2026-08-16 18:41:37 +0300

    logistic-regression. Other fixes

--- ml_fundamentals/linear_models/logistic_regression/implementations/gradient_descent.py
+++ ml_fundamentals/linear_models/logistic_regression/implementations/gradient_descent.py
@@ -7,10 +7,10 @@ class GradientDescentLogisticRegression:
         self.learning_rate = lr
         self.eps = eps
 
-        self.w: np.ndarray = np.zeros((in_features + 1, 1))
+        self.w: np.ndarray = np.zeros((in_features + 1, 1), dtype=np.float32)
 
     def fit(self, X: np.ndarray, y: np.ndarray, max_iter: int = 1000) -> np.ndarray:
-        ones = np.ones((X.shape[0], 1))
+        ones = np.ones((X.shape[0], 1), dtype=X.dtype)
         X = np.hstack((X, ones))
         y = y.reshape(-1, 1)
 
@@ -24,11 +24,13 @@ class GradientDescentLogisticRegression:
 
         return self.w
 
-    def predict(self, X: np.ndarray) -> np.ndarray:
-        ones = np.ones((X.shape[0], 1))
+    def predict_proba(self, X: np.ndarray) -> np.ndarray:
+        ones = np.ones((X.shape[0], 1), dtype=X.dtype)
         X = np.hstack((X, ones))
-
         return np.ravel(self._sigmoid(X @ self.w))
+
+    def predict(self, X: np.ndarray, threshold: float = 0.5) -> np.ndarray:
+        return (self.predict_proba(X) >= threshold).astype(np.int64)
 
     def _gradient(self, X: np.ndarray, y: np.ndarray) -> np.ndarray:
         p = self._sigmoid(X @ self.w)
@@ -38,7 +40,7 @@ class GradientDescentLogisticRegression:
         return 1 / (1 + np.exp(-X))
 
     @staticmethod
-    def bce_loss(y_true: np.ndarray, y_pred: np.ndarray) -> float:
-        pos_class = y_true * np.log(y_pred)
-        neg_class = (1 - y_true) * np.log(1 - y_pred)
+    def bce_loss(y_true: np.ndarray, y_pred: np.ndarray, eps: float = 1e-6) -> float:
+        pos_class = y_true * np.log(y_pred + eps)
+        neg_class = (1 - y_true) * np.log(1 - y_pred + eps)
         return -np.mean(pos_class + neg_class)
